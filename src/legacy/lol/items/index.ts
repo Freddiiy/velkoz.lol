2da432efe5c1af41f3a8e4745dcbffe33d43d440
Freddiiy <frederikgaller@live.dk>
2022-10-31 20:25:53 +0100

    legacy removed keeps ruining my build

--- src/legacy/lol/items/index.ts
+++
@@ -1,9 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {ddragon} from "reksai";
-import {IItem} from "reksai/src/@types/items";
-
-
-export default async (req: NextApiRequest, res: NextApiResponse) =>  {
-    const item: IItem = await ddragon.item.all()
-    res.status(200).json(item)
-}
--- src/legacy/lol/spells/index.ts
+++
@@ -1,7 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {summonerSpells} from "../../../data/sumonerSpells";
-
-
-export default async (req: NextApiRequest, res: NextApiResponse) =>  {
-    res.status(200).json(summonerSpells)
-}
