ceb55844b693ef86005f315c0ad6e484da78efdf
Freddiiy <frederikgaller@live.dk>
2022-11-02 16:00:45 +0100

    asd

--- src/views/lol/summoner/Match.tsx
+++ src/views/lol/summoner/Match.tsx
@@ -26,7 +26,7 @@ const Match = ({match, summoner}: { match: TMatch; summoner: TSummoner | undefin
 		(e) => e.puuid == summoner?.puuid
 	);
 
-	console.log(sumInfo)
+
 	const win = sumInfo?.win;
 	const winBorder = win ? "border-blue-400" : "border-red-500";
 	const winText = win ? "text-blue-400" : "text-red-500";
@@ -36,8 +36,6 @@ const Match = ({match, summoner}: { match: TMatch; summoner: TSummoner | undefin
 
 	const keyStoneId = sumInfo?.perks?.styles[0].selections[0].perk;
 	const secondRune = sumInfo?.perks?.styles[1].style;
-
-	console.log(keyStoneId)
 
 	const champ = champContext?.find(
 		(c) => parseInt(c.key) == sumInfo?.championId
